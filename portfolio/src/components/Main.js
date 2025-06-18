11d639d93cb85195f25c234c50a3fb7af066977e
MateoRosario <mateo.rosarino22@gmail.com>
2025-06-18 20:27:43 -0300

    practicas de js, y se comento el header y el footer

---
+++ portfolio/src/components/Main.js
@@ -0,0 +1,27 @@
+/* function sumarArreglo(numeros) {
+  let suma = 0;
+  for (let i = 0; i < numeros.length; i++) {
+    suma += [i];
+  }
+  return suma;
+}
+let numeros = [1, 3, 5, 6];
+console.log(sumarArreglo(numeros));
+ */
+/* class usuario {
+  constructor(nombre, edad, esAlumno) {
+    this.nombre = nombre;
+    this.edad = edad;
+    this.esAlumno = esAlumno;
+  }
+  ingresar() {
+    if (this.esAlumno) {
+      return `${this.nombre} a ingresado al complejo.`;
+    } else return `${this.nombre} a inresago al trabajo`;
+  }
+}
+let usuario1 = new usuario(Lucas, 25, esAlumno);
+{
+  usuario1.ingresar;
+  3;
+} */
